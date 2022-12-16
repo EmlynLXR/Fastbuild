1018298edb4d87256cdb6e6e3bd0d0f09e06b5ef
AllenKenway <1021551729@qq.com>
2022-12-16 11:40:48 +0800

    update .sh

--- test_quiet.sh
+++ test_quiet.sh
@@ -11,7 +11,7 @@ RUN(){
     re=`docker system prune -af`
 
     echo "[INFO]Warm up " $1 " ..."
-    time docker build --build-arg http_proxy=http://${host}:8080 --build-arg HTTP_PROXY=http://${host}:8080 -t test -f Dockerfile .
+    re=`time docker build -q --build-arg http_proxy=http://${host}:8080 --build-arg HTTP_PROXY=http://${host}:8080 -t test -f Dockerfile .`
 
     for ((i=1; i<=${loop}; i++))do      
         while true;do
@@ -19,9 +19,15 @@ RUN(){
             re=`docker rmi -f test`  
             echo "[INFO]Start " $1 "-" ${i} " default test ..."
             startTraffic1=`cat /proc/net/dev | grep enp | awk '{print $2}'`
-            time docker build -t test -f Dockerfile . 
+            re=`time docker build -q -t test -f Dockerfile . `
             endTraffic1=`cat /proc/net/dev | grep enp | awk '{print $2}'`
             echo "[INFO]Test " $1 "-" ${i} ", default = " $((endTraffic1 - startTraffic1)) 
+            if [[ $re =~ "sha256" ]];then
+                echo $re
+                break
+            else
+                echo "Error occurs in default test, possibly dur to unstable network, the image build will be restart ..."
+            fi
         done
     done
 
@@ -31,9 +37,15 @@ RUN(){
             re=`docker rmi -f test`
             echo "[INFO]Start " $1 "-" ${i} " cached test ..."
             startTraffic2=`cat /proc/net/dev | grep enp | awk '{print $2}'`
-            time docker build --build-arg http_proxy=http://${host}:8080 --build-arg HTTP_PROXY=http://${host}:8080 -t test -f Dockerfile .
+            re=`time docker build -q --build-arg http_proxy=http://${host}:8080 --build-arg HTTP_PROXY=http://${host}:8080 -t test -f Dockerfile .`
             endTraffic2=`cat /proc/net/dev | grep enp | awk '{print $2}'`
             echo "[INFO]Test " $1 "-" ${i} ", cached = " $((endTraffic2 - startTraffic2)) 
+            if [[ $re =~ "sha256" ]];then
+                echo $re
+                break
+            else
+                echo "Error occurs in cached test, possibly dur to unstable network, the image build will be restart ..."
+            fi
         done
     done
 }
